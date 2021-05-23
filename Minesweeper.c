4867875f4895fa3384985fd1666851ca48eb276b
Tirth Nagar <tirthnagar.n1@gmail.com>
2021-05-23 12:25:28 -0400

    Update Borders

--- Minesweeper.c
+++ Minesweeper.c
@@ -3,17 +3,6 @@
 #include <unistd.h>
 #include <string.h>
 #include <curses.h>
-#include <sys/ioctl.h>
-
-
-int terminalSize(void){
-	
-	struct winsize ws;
-	ioctl(STDIN_FILENO, TIOCGWINSZ, &ws);
-	
-	return ws.ws_row,ws.ws_col;
-}
-
 
 int main() {
 	
@@ -24,14 +13,15 @@ int main() {
 	int start_y=0,start_x=0;
 	
 	start_color();
-	init_pair(1, 2, 0);
-	init_pair(2, 1, 0);
-	init_pair(3, 5, 0);
-	init_pair(4, 6, 0);
-	init_pair(5, 0, 3);
-	init_pair(6, 7, 0);
+	init_pair(1, COLOR_GREEN, COLOR_BLACK);
+	init_pair(2, COLOR_RED, COLOR_BLACK);
+	init_pair(3, COLOR_MAGENTA, COLOR_BLACK);
+	init_pair(4, COLOR_CYAN, COLOR_BLACK);
+	init_pair(5, COLOR_BLACK, COLOR_YELLOW);
+	init_pair(6, COLOR_WHITE, COLOR_BLACK);
 	
-	int width,height = terminalSize();
+	int width = 0,height = 0;
+	getmaxyx(stdscr,height,width);
 	
 	
 	WINDOW * win = newwin(height,width,start_y,start_x);
@@ -47,10 +37,7 @@ int main() {
 		attroff(COLOR_PAIR(1));
 		wrefresh(win);
 	}
-	//attron(COLOR_PAIR(7));
-	//wborder(win, ' ',' ',' ',' ',' ',' ',' ',' ');
-	//attroff(COLOR_PAIR(7));
-	//wrefresh(win);
+
     fclose (p);
 	
 	start_y = 8, start_x = 0;
@@ -58,10 +45,8 @@ int main() {
 	WINDOW * opt = newwin(height-start_y,width,start_y,start_x);
 	refresh();
 	
-	attron(A_BOLD);
-	box(opt,0,0); //Doesn't show bottom border
-	//wborder(opt, '*','*','*','*','*','*','*','*'); //Doesn't show bottom border
-	attroff(A_BOLD);
+	box(opt,0,0);
+	
 	wrefresh(opt);
 
 	getch();
