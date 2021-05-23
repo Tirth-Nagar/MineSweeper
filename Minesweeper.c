23ef70cb4bf322e18b7130711d39a7069e42b5f6
Tirth Nagar <tirthnagar.n1@gmail.com>
2021-05-23 11:51:16 -0400

    Updates

--- Minesweeper.c
+++ Minesweeper.c
@@ -21,22 +21,20 @@ int main() {
 	refresh();
 	cbreak();
 	
-	int color = 1;
+	int start_y=0,start_x=0;
 	
 	start_color();
-	init_pair(1, COLOR_GREEN, COLOR_BLACK);
-	init_pair(2, COLOR_RED, COLOR_BLACK);
-	init_pair(3, COLOR_MAGENTA, COLOR_BLACK);
-	init_pair(4, COLOR_CYAN, COLOR_BLACK);
-	init_pair(5, COLOR_BLACK, COLOR_YELLOW);
-	init_pair(6, COLOR_WHITE, COLOR_BLACK);
-	attron(COLOR_PAIR(color));
-	
+	init_pair(1, 2, 0);
+	init_pair(2, 1, 0);
+	init_pair(3, 5, 0);
+	init_pair(4, 6, 0);
+	init_pair(5, 0, 3);
+	init_pair(6, 7, 0);
 	
 	int width,height = terminalSize();
 	
 	
-	WINDOW * win = newwin(height,width,0,0);
+	WINDOW * win = newwin(height,width,start_y,start_x);
 	refresh();
 	
 	FILE *p = fopen ("logo.txt" , "r");;
@@ -44,20 +42,29 @@ int main() {
 	int i = 1;
 
 	while(fgets (str, 200, p) != NULL){
+		attron(COLOR_PAIR(1));
 		mvprintw(i++, COLS / 2 - 50, "%s", str);
+		attroff(COLOR_PAIR(1));
 		wrefresh(win);
 	}
-	//attron(A_STANDOUT);
+	//attron(COLOR_PAIR(7));
 	//wborder(win, ' ',' ',' ',' ',' ',' ',' ',' ');
-	//attroff(A_STANDOUT);
+	//attroff(COLOR_PAIR(7));
 	//wrefresh(win);
     fclose (p);
 	
-	attron(COLOR_PAIR(4));
-	wborder(win, '*','*','*','*','*','*','*','*'); //Doesn't show bottom border
-	attroff(COLOR_PAIR(4));
-	wrefresh(win);
+	start_y = 8, start_x = 0;
+	
+	WINDOW * opt = newwin(height-start_y,width,start_y,start_x);
+	refresh();
+	
+	attron(A_BOLD);
+	box(opt,0,0); //Doesn't show bottom border
+	//wborder(opt, '*','*','*','*','*','*','*','*'); //Doesn't show bottom border
+	attroff(A_BOLD);
+	wrefresh(opt);
 
+	getch();
 	getch();
 	
 	endwin();
